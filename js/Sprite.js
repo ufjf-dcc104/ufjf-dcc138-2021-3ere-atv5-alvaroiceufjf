83224286afdba193aa3499b90f67e06240447913
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2022-02-13 22:43:15 -0300

    Criação de classe Sprite

---
+++ js/Sprite.js
@@ -0,0 +1,33 @@
+export default class Sprite{
+
+    //Modela algo que se move na tela
+    constructor({x=100,y=100,w=20,h=20,color="white",vx=0,vy=0}={}){
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+
+    }
+
+    desenhar(ctx){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x,this.y,this.w,this.h);
+
+    }
+
+    passo(dt){
+        this.x = this.x + this.vx*dt;
+        this.y = this.y + this.vy*dt;
+    }
+    colidiuCom(outro){
+        return !(
+            this.x > outro.x + outro.w ||
+            this.x + this.w < outro.x ||
+            this.y > outro.y + outro.h ||
+            this.y + this.h < outro.y
+        );
+    }
+}  
